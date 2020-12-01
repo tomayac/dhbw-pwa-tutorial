9f90d3a62da0c037fc82bf7a58cc370c1341a87b
tomayac <steiner.thomas@gmail.com>
2020-12-01 15:26:52 +0100

    Timestamp 15:26

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const ctx = canvas.getContext('2d', {
 });
 
 const clear = () => {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'black';
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
 };
 clear();
 
-const btnClear =document.querySelector('#clear');
+const btnClear = document.querySelector('#clear');
 btnClear.addEventListener('click', clear);
 
 let previousPoint = null;
@@ -42,9 +42,64 @@ txtColor.addEventListener('change', () => {
   ctx.fillStyle = txtColor.value;
 });
 
+const fileOptions = {
+  types: [
+    {
+      description: 'PNG files',
+      accept: { 'image/png': ['.png'] },
+    },
+    {
+      description: 'JPG files',
+      accept: { 'image/jpeg': ['.jpg', '.jpeg'] },
+    },
+  ],
+};
+
+const btnSave = document.querySelector('#save');
+btnSave.addEventListener('click', async () => {
+  const blob = await toBlob(canvas);
+  if ('showSaveFilePicker' in window) {
+    const handle = await window.showSaveFilePicker(fileOptions);
+    const writable = await handle.createWritable();
+    await writable.write(blob);
+    await writable.close();
+  } else {
+    const anchor = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    anchor.href = url;
+    anchor.download = '';
+    anchor.click();
+    URL.revokeObjectURL(url);
+  }
+});
+
+const btnOpen = document.querySelector('#open');
+btnOpen.addEventListener('click', async () => {
+  if ('showOpenFilePicker' in window) {
+    const [handle] = await window.showOpenFilePicker(fileOptions);
+    const file = await handle.getFile();
+    const image = await getImage(file);
+    ctx.drawImage(image, 0, 0);
+  } else {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/png,image/jpeg,.png,.jpg,.jpeg,.webp,.gif';
+    input.addEventListener('change', async () => {
+      try {
+        const blob = input.files[0];
+        const image = await getImage(blob);
+        ctx.drawImage(image, 0, 0);
+      } catch (e) {
+        console.error(e.message, e.name);
+      }
+    });
+    input.click();
+  }
+});
+
 window.addEventListener('load', async () => {
-    if ('serviceWorker' in navigator) {
-        const registration = await navigator.serviceWorker.register('sw.js');
-        console.log(`Service worker registered for ${registration.scope}.`);
-    }
+  if ('serviceWorker' in navigator) {
+    const registration = await navigator.serviceWorker.register('sw.js');
+    console.log(`Service worker registered for ${registration.scope}.`);
+  }
 });
--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 console.log('Hello from service worker');
-
+// 5
 const cacheName = 'offline-cache';
 
 const files = [
@@ -10,37 +10,43 @@ const files = [
   'helpers.js',
   'icon.png',
   'style.css',
-  'manifest.webmanifest'
+  'manifest.webmanifest',
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    self.skipWaiting();
-    const cache = await caches.open(cacheName);
-    await cache.addAll(files);
-  })());
+  event.waitUntil(
+    (async () => {
+      self.skipWaiting();
+      const cache = await caches.open(cacheName);
+      await cache.addAll(files);
+    })()
+  );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    self.clients.claim();
-  })());
+  event.waitUntil(
+    (async () => {
+      self.clients.claim();
+    })()
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith((async () => {
-    const url = event.request.url;
-    const cache = await caches.open(cacheName);
-    try {
-      const networkResponse = await fetch(url);
-      if (networkResponse.ok) {
-        console.log(`From network: ${url}.`);
-        return networkResponse;
+  event.respondWith(
+    (async () => {
+      const url = event.request.url;
+      const cache = await caches.open(cacheName);
+      try {
+        const networkResponse = await fetch(url);
+        if (networkResponse.ok) {
+          console.log(`From network: ${url}.`);
+          return networkResponse;
+        }
+        throw Error(`Fetch failed with status ${networkResponse.status}`);
+      } catch (err) {
+        console.log(`From cache: ${url}.`);
+        return cache.match(url);
       }
-      throw Error(`Fetch failed with status ${networkResponse.status}`);
-    } catch(err) {
-      console.log(`From cache: ${url}.`);
-      return cache.match(url);
-    }
-  })());
+    })()
+  );
 });
